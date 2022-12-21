69baed30753e77eb4a731c2586f73f6c01a46da8
Mauriccio <mauriccio@MacBook-Pro-van-Mauriccio.local>
2022-12-21 12:27:37 +0100

    client/added button variant 'IconButton'

---
+++ client/src/components/atoms/Button/IconButton.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+import BaseButton, { IBaseButton } from "./BaseButton";
+
+interface IIconButton extends IBaseButton {
+  icon?: JSX.Element;
+  hasBorder?: boolean;
+}
+
+const SIconButton = styled(BaseButton)<IIconButton>`
+  border: ${(props) => (props.hasBorder ? "1px solid black" : "none")};
+  width: 84px;
+  height: 84px;
+  
+  & svg {
+    min-width: 32px;
+    min-height: 32px;
+
+    & path {
+      fill: black;
+    }
+  }
+`;
+
+const IconButton = ({ icon, hasBorder, ...rest }: IIconButton) => {
+  return <SIconButton children={icon} hasBorder={hasBorder} {...rest} />;
+};
+
+export default IconButton;
