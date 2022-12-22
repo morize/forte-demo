8f723d0d844e48cba39eb3215e3c547d8c35321e
morize <mauricemr@outlook.com>
2022-12-22 16:10:52 +0100

    client/placed talent components in showcase environment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
-import styled from "styled-components";
+import { useState } from 'react';
+import styled from 'styled-components';
 
-import IconButton from "./components/atoms/Button/IconButton";
-import BaseButton from "./components/atoms/Button/BaseButton";
-import DropdownButton from "./components/atoms/Button/DropdownButton";
+import IconButton from './components/atoms/Button/IconButton';
+import BaseButton from './components/atoms/Button/BaseButton';
+import DropdownButton from './components/atoms/Button/DropdownButton';
 
-import HeaderTitle from "./components/atoms/HeaderTitle/HeaderTitle";
+import HeaderTitle from './components/atoms/HeaderTitle/HeaderTitle';
 
-import { MenuIcon } from "./components/atoms/Icons/Icons";
+import Talent from './components/atoms/Talent/Talent';
+
+import { MenuIcon } from './components/atoms/Icons/Icons';
 
 const SButtonContainer = styled.div`
   & button {
@@ -18,6 +20,15 @@ const SButtonContainer = styled.div`
 const SHeaderContainer = styled.div`
   display: flex;
   justify-content: center;
+`;
+
+const STalentsContainer = styled.div`
+  display: grid;
+  justify-items: center;
+  width: 380px;
+  grid-gap: 24px;
+  grid-template-columns: repeat(2, auto);
+  grid-template-rows: repeat(2, auto);
 `;
 
 function App() {
@@ -39,6 +50,19 @@ function App() {
         <DropdownButton label="Beinvloeden" />
         <BaseButton label="Bekijk jouw talentenprofiel" />
       </SButtonContainer>
+
+      <STalentsContainer>
+        <Talent
+          talent="Verantwoordelijkheidsbesef"
+          domain="executing"
+          placement={1}
+        />
+        <Talent talent="Hersteldrang" domain="executing" placement={2} />
+        <Talent talent="Leergierigheid" domain="strategic_thinking" placement={3} />
+        <Talent talent="Input" domain="strategic_thinking" placement={4} />
+        <Talent talent="Onpartijdheid" domain="executing" placement={5} />
+        <Talent talent="Maximalisering" domain="influencing" placement={6} />
+      </STalentsContainer>
     </>
   );
 }
