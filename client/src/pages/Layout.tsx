bc4567e254e1a35b46b4bbd21b25ce36d07b71c6
Mauriccio Mendoza Reyes <mauriccio.mendozareyes@synetic.nl>
2022-12-31 12:53:33 +0100

    client/added default layout for the talent information page

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -9,7 +9,10 @@ interface ILayout {
 }
 
 const SLayout = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 800px;
+  height: calc(100vh - 100px);
   margin: auto;
 
   & > div {
@@ -32,6 +35,11 @@ const SLayout = styled.div`
 
   @media (max-width: 900px) {
     width: 80%;
+    height: calc(100vh - 80px);
+
+    & > div:last-child {
+      margin-bottom: 12px;
+    }
   }
 `;
 
@@ -41,11 +49,33 @@ const Layout = ({ children }: ILayout) => {
 
   const paths = location.pathname.split('/').filter((path) => path !== '');
   const pathLength = paths.length;
-  
+
+  const determineHeader = () => {
+    switch (pathLength) {
+      case 3:
+        return {
+          title: paths[pathLength - 2],
+          subtitle: paths[pathLength - 1],
+        };
+
+      case 4:
+        return {
+          title: paths[pathLength - 3],
+          subtitle: paths[pathLength - 2],
+        };
+      default:
+        return {
+          title: paths[pathLength - 1],
+          subtitle: undefined,
+        };
+    }
+  };
+
   return (
     <>
       <PageNavigation
-        pageTitle={paths[pathLength - 1]}
+        pageTitle={determineHeader().title}
+        pageSubtitle={determineHeader().subtitle}
         hasBackButton={pathLength > 1}
         onBackButtonClicked={(e) => {
           e.preventDefault();
