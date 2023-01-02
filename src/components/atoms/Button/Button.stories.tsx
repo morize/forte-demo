0562c51a6d8dfcfb032cd25da3cb7bdee09d1f1c
Mauriccio Mendoza Reyes <mauriccio.mendozareyes@synetic.nl>
2023-01-02 14:44:09 +0100

    button atom: removed selectbutton variant from atom group

--- src/components/atoms/Button/Button.stories.tsx
+++ src/components/atoms/Button/Button.stories.tsx
@@ -1,16 +1,14 @@
-import { useState } from 'react';
-
 import BaseButton, { IBaseButton } from './BaseButton';
-import SelectButton from './SelectButton';
 import IconButton, { IIconButton } from './IconButton';
 
-import { Menu, ExpandMore } from '../../atoms/Icons/Icons';
+import { Menu } from '../../atoms/Icons/Icons';
 
 export default {
   title: 'Atoms',
 };
 
 export const ButtonBase = (args: IBaseButton) => <BaseButton {...args} />;
+
 ButtonBase.story = {
   name: 'Base Button',
 };
@@ -19,6 +17,7 @@ ButtonBase.args = {
 };
 
 export const ButtonIcon = (args: IIconButton) => <IconButton {...args} />;
+
 ButtonIcon.story = {
   name: 'Icon Button',
 };
@@ -26,31 +25,5 @@ ButtonIcon.story = {
 ButtonIcon.args = {
   label: 'Menu',
   icon: <Menu />,
-};
-
-type OptionType = { label: string; value: string };
-
-export const ButtonSelect = () => {
-  const [selectValue, setSelectValue] = useState({
-    label: 'Alles',
-    value: 'alles',
-  });
-
-  return (
-    <SelectButton
-      value={selectValue}
-      onChange={(option) => setSelectValue(option as OptionType)}
-      options={[
-        { label: 'Alles', value: 'alles' },
-        { label: 'Uitvoeren', value: 'uitvoeren' },
-        { label: 'Beinvloeden', value: 'beinvloeden' },
-        { label: 'Relatie Bouwen', value: 'relatie_bouwen' },
-        { label: 'Strategisch Denken', value: 'strategisch_denken' },
-      ]}
-    />
-  );
-};
-
-ButtonSelect.story = {
-  name: 'Select Button',
+  hasBorder: true,
 };
