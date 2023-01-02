7c91c215888f0045e5c55b5f84d17cc0dc881a06
Mauriccio Mendoza Reyes <mauriccio.mendozareyes@synetic.nl>
2023-01-02 17:18:10 +0100

    replaced default with 'alles' which is the default type inside the seeder

--- src/components/atoms/Colors/Colors.tsx
+++ src/components/atoms/Colors/Colors.tsx
@@ -3,5 +3,5 @@ export const TalentDomainsRGB: { [key: string]: number[] } = {
   beinvloeden: [233, 114, 0],
   'relatie bouwen': [0, 112, 205],
   'strategisch denken': [0, 148, 93],
-  default: [0, 0, 0],
+  alles: [0, 0, 0],
 };
