fda37b410a28de0f94e19f29aed13117870b9729
Mauriccio Mendoza Reyes <mauriccio.mendozareyes@synetic.nl>
2023-01-02 17:46:05 +0100

    added media tag for a mobile view of the navigation items in storybook

--- src/components/molecules/TalentInfoNavigation/TalentInfoNavigation.stories.tsx
+++ src/components/molecules/TalentInfoNavigation/TalentInfoNavigation.stories.tsx
@@ -1,15 +1,27 @@
+import styled from 'styled-components';
+
 import TalentInfoNavigation from './TalentInfoNavigation';
 
 export default {
   title: 'Molecules',
 };
 
+const Container = styled.div`
+  width: 800px;
+  margin: auto;
+
+  @media (max-width: 900px) {
+    width: 100%;
+  }
+`;
 export const TalentInfoNavigationItem = () => (
-  <TalentInfoNavigation
-    onNavigationItemClicked={(navigationValue) => {
-      console.log(navigationValue);
-    }}
-  />
+  <Container>
+    <TalentInfoNavigation
+      onNavigationItemClicked={(navigationValue) => {
+        console.log(navigationValue);
+      }}
+    />
+  </Container>
 );
 
 TalentInfoNavigationItem.story = {
